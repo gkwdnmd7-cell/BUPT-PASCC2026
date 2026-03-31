13eda86d335be976975d4d331e08a289abf79f21
Galaxy-horn <1697807685@qq.com>
2026-03-31 14:07:54 +0800

    dlh的测试

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,21 +1,21 @@
 # CMake generated Testfile for 
-# Source directory: E:/金介然/大三下/编译课设/CODE
-# Build directory: E:/金介然/大三下/编译课设/CODE/build
+# Source directory: C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026
+# Build directory: C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-  add_test(lexer_tests "E:/金介然/大三下/编译课设/CODE/build/Debug/lexer_tests.exe")
-  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;25;add_test;E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;0;")
+  add_test(lexer_tests "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/Debug/lexer_tests.exe")
+  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;25;add_test;C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;0;")
 elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-  add_test(lexer_tests "E:/金介然/大三下/编译课设/CODE/build/Release/lexer_tests.exe")
-  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;25;add_test;E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;0;")
+  add_test(lexer_tests "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/Release/lexer_tests.exe")
+  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;25;add_test;C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;0;")
 elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-  add_test(lexer_tests "E:/金介然/大三下/编译课设/CODE/build/MinSizeRel/lexer_tests.exe")
-  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;25;add_test;E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;0;")
+  add_test(lexer_tests "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/MinSizeRel/lexer_tests.exe")
+  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;25;add_test;C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;0;")
 elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-  add_test(lexer_tests "E:/金介然/大三下/编译课设/CODE/build/RelWithDebInfo/lexer_tests.exe")
-  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;25;add_test;E:/金介然/大三下/编译课设/CODE/CMakeLists.txt;0;")
+  add_test(lexer_tests "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/RelWithDebInfo/lexer_tests.exe")
+  set_tests_properties(lexer_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;25;add_test;C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/CMakeLists.txt;0;")
 else()
   add_test(lexer_tests NOT_AVAILABLE)
 endif()
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/金介然/大三下/编译课设/CODE
+# Install script for directory: C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/金介然/大三下/编译课设/CODE/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/金介然/大三下/编译课设/CODE/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/16978/Desktop/大三下/编译原理课设/BUPT-PASCC2026/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
